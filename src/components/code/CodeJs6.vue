91c712be82dbf978248117c29f893081fef5f45f
Lee <lixiaoyi@doov.com.cn>
2024-04-17 11:38:52 +0800

    add code

--- src/components/code/CodeJs6.vue
+++ src/components/code/CodeJs6.vue
@@ -1,113 +1,105 @@
 <script setup lang = "ts" name = "CodeJs6">
 import {nextTick, onMounted, ref, reactive, toRefs} from "vue";
 import {basicSetup} from 'codemirror';
-import {EditorView, keymap} from "@codemirror/view"
+import {EditorView, keymap} from "@codemirror/view";
+import {EditorState, Compartment} from "@codemirror/state";
 import {historyKeymap, history} from "@codemirror/history";
 import {insertTab, standardKeymap} from "@codemirror/commands";
-import {autocompletion} from '@codemirror/autocomplete';
+import {autocompletion, startCompletion} from '@codemirror/autocomplete';
 import {python} from "@codemirror/lang-python";
 import {javascript} from "@codemirror/lang-javascript";
 import {java} from "@codemirror/lang-java";
 import {json} from "@codemirror/lang-json";
+import {yaml} from "@codemirror/lang-yaml";
+import {css} from "@codemirror/lang-css";
+import {vue} from "@codemirror/lang-vue";
+import {html} from "@codemirror/lang-html";
+import {cpp} from "@codemirror/lang-cpp";
+import {xml} from "@codemirror/lang-xml";
+import {markdown} from "@codemirror/lang-markdown";
+import {go} from "@codemirror/lang-go";
 import {MariaSQL, MSSQL, MySQL, PLSQL, PostgreSQL, sql, StandardSQL} from "@codemirror/lang-sql";
-import {oneDarkHighlightStyle, oneDarkTheme} from '@codemirror/theme-one-dark';
-import {lineNumbers} from "@codemirror/gutter";
+import {birdsOfParadise, boysAndGirls, amy, ayuLight, bespin, clouds, barf, dracula} from 'thememirror';
 
 const model = defineModel("value", {default: ""});
+const editorLang = ref<any>(javascript());
+const editorTheme = ref<any>(dracula);
 const props = defineProps({
   funcName: {type: String, default: ""},
   fieldName: {type: String, default: ""},
-  lang: {type: String, default: "sql"},
-  theme: {type: String, default: "monokai"},
+  lang: {type: String, default: null},
+  theme: {type: String, default: "dracula"},
   boxHeight: {type: String, default: "95%"}
 });
+const languageConf = new Compartment;
+const completionList = ref<any>([]);
+const languages = ref<Array<any>>([
+  {name: "Java", value: "java", obj: java(), icon: "setting"},
+  {name: "Python", value: "python", obj: python(), icon: "setting"},
+  {name: "JSON", value: "json", obj: json(), icon: "setting"},
+  {name: "Sql", value: "sql", obj: sql({tables: [{label: 'sys_menusinfo', apply: 'sys_menusinfo', detail: 'String'}]}), icon: "setting"},
+  {name: "JavaScript", value: "javascript", obj: javascript(), icon: "setting"},
+  {name: "Vue", value: "vue", obj: vue(), icon: "setting"},
+  {name: "Css", value: "css", obj: css(), icon: "setting"},
+  {name: "YAML", value: "yaml", obj: yaml(), icon: "setting"},
+  {name: "XML", value: "xml", obj: xml(), icon: "setting"},
+  {name: "Markdown", value: "markdown", obj: markdown(), icon: "setting"},
+  {name: "Go", value: "go", obj: go(), icon: "setting"},
+  {name: "C++", value: "cpp", obj: cpp(), icon: "setting"},
+  {name: "Html", value: "html", obj: html(), icon: "setting"},
+]);
+const themesConf = new Compartment;
+const themes = ref<Array<any>>([
+  {name: "BirdsOfParadise", value: "birdsOfParadise", obj: birdsOfParadise, icon: "setting"},
+  {name: "BoysAndGirls", value: "boysAndGirls", obj: boysAndGirls, icon: "setting"},
+  {name: "Amy", value: "amy", obj: amy, icon: "setting"},
+  {name: "AyuLight", value: "ayuLight", obj: ayuLight, icon: "setting"},
+  {name: "Bespin", value: "bespin", obj: bespin, icon: "setting"},
+  {name: "Clouds", value: "clouds", obj: clouds, icon: "setting"},
+  {name: "Barf", value: "barf", obj: barf, icon: "setting"},
+  {name: "Dracula", value: "dracula", obj: dracula, icon: "setting"},
+
+]);
 const editor = ref<EditorView>();
 const codemirror = ref();
 
-const allKeyList = ref([]);
-const groupList = ref([]);
-const group = ref("");
-const data = reactive({
-  form: {},
-});
-const {form} = toRefs(data);
-
-// 获取自定义提示内容
-const getCommandList = () => {
-  groupList.value = [
-    {id: '1', label: '分组1', value: 'group1'},
-    {id: '2', label: '分组2', value: 'group2'},
-  ];
-  allKeyList.value = [
-    {label: "match", type: "keyword", apply: "match", detail: "match"},
-    {label: "hello", type: "variable", apply: "hello", detail: "hellodetail"},
-    {label: "magic", type: "text", apply: "⠁⭒*.✩.*⭒⠁", detail: "macro"},
-  ];
-}
-
-// 自定义的代码不全，options为自定义内容，以@开头进行匹配
-const myCompletions = (context) => {
-  let word = context.matchBefore(/\w*/);
-  if (!word && !context.explicit) return null;
-  console.log(word);
-  return {
-    from: word.from ? word.from : context.pos,
-    options: allKeyList.value,
-  };
-}
-
-// 选择分组添加到编辑其中
-const insertGroup = () => {
-  insertCommandContant(group.value);
-  group.value = "";
-}
-
-// 外部输入内容，添加到编辑器当前光标(或选中内容)所在的位置
-const insertCommandContant = (insertContent: any) => {
-  editor.value.dispatch({
-    changes: {
-      from: editor.value.state.selection.ranges[0].from,
-      to: editor.value.state.selection.ranges[0].to,
-      insert: insertContent
-    },
+const customerTheme = (theme: string) => {
+  let fdata = themes.value.find(item => item.value == theme);
+  editorTheme.value = fdata.obj;
+  editor.value?.dispatch({
+    effects: themesConf.reconfigure(editorTheme.value)
   });
-}
-const sqlType = {
-  "mysql": MySQL,
-  "mariadb": MariaSQL,
-  "postgrel": PostgreSQL,
-  "mssql": MSSQL,
-  "oracle": PLSQL
 };
-const dialect = () => {
 
-};
 const currentValFun = (val: string) => {
-  console.log(val);
+  // console.log(val);
 };
-const langInfo = () => {
-  if (props.lang == "javascript") {
-    return javascript();
-  } else if (props.lang == "json") {
-    return json();
-  } else if (props.lang == "python") {
-    return python();
-  } else if (props.lang == "java") {
-    return java();
-  } else {
-    // 如果是
-    return sql({
-      dialect: sqlType[props.lang] || StandardSQL,
-      schema: []
-    });
+/**
+ * 获取语言信息
+ * @param lang
+ */
+const langInfo = (lang: string) => {
+  let fdata = languages.value.find(item => item.value == lang);
+  editorLang.value = fdata.obj;
+  editor.value?.dispatch({
+    effects: languageConf.reconfigure(editorLang.value)
+  });
+};
+const updateList = (args: any) => {
+  let change = args?.changes?.inserted[1]?.text[0];
+  console.log(change);
+  let changes = args?.changes?.sections[0];
+  if (change == ".") {
+    startCompletion(editor.value);
   }
-};
+}
+const docCompletions = () => {
 
+};
 /**
  * 初始化
  */
 const init = async () => {
-  getCommandList();
   if (typeof editor.value !== 'undefined') {
     if (editor.value instanceof EditorView) {
       editor.value.destroy();
@@ -119,15 +111,15 @@ const init = async () => {
     doc: model.value,
     extensions: [
       basicSetup,
-      oneDarkTheme,
+      themesConf.of(editorTheme.value),
       keymap.of([standardKeymap, historyKeymap, {key: "Tab", run: insertTab}]),
-      langInfo(),
-      autocompletion(),
-      autocompletion({override: [myCompletions]}),
+      languageConf.of(editorLang.value),
+      autocompletion({activateOnTyping: true}),
       EditorView.updateListener.of((v) => {
+        updateList(v);
         if (v.docChanged) {
           model.value = v.state.doc.toString();
-          currentValFun(v.state.doc.toString());
+          currentValFun(model.value);
         }
       })
     ],
@@ -136,16 +128,73 @@ const init = async () => {
 };
 onMounted(async () => {
   await nextTick(() => {
+    langInfo(props.lang || "javascript");
     init();
   });
+});
+
+function myCompletions(context: CompletionContext) {
+  let word = context.matchBefore(/\w*/)
+  if (word.from == word.to && !context.explicit)
+    return null
+  return {
+    from: word.from,
+    options: completionList.value
+  }
+};
+const setAutoCompletion = (val: object) => {
+  completionList.value.push(...Object.keys(val).map(item =>
+      ({
+        label: item,
+        apply: item,
+        detail: "String",
+      })
+  ));
+  console.log(completionList.value);
+};
+defineExpose({
+  editor, setAutoCompletion
 })
 </script>
 <template>
+  <el-dropdown>
+    <span class = "el-dropdown-link">
+      主题
+      <el-icon class = "el-icon--right">
+        <arrow-down/>
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click = "customerTheme(item.value)" v-for = "item in themes">
+          <star-horse-icon :icon-class = "item.icon" color = "gray" size = "14px"/>
+          {{ item.name }}
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+  <el-dropdown v-if = "!lang">
+    <span class = "el-dropdown-link">
+      语言
+      <el-icon class = "el-icon--right">
+        <arrow-down/>
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click = "langInfo(item.value)" v-for = "item in languages">
+          <star-horse-icon :icon-class = "item.icon" color = "gray" size = "14px"/>
+          {{ item.name }}
+        </el-dropdown-item>
+
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
   <div ref = "codemirror" class = "coder"></div>
 </template>
 <style lang = "scss" scoped>
 .coder {
-  font-size: 16px;
+  font-size: 17px;
   width: 100%;
   height: v-bind(boxHeight);
 
