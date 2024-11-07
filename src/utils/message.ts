9d9b5dd618d64aab8828527e1308b6ff02731235
lixy <l_1019@163.com>
2024-11-07 09:57:07 +0800

    add code

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -1,4 +1,6 @@
-import {ElMessage, ElMessageBox} from "element-plus";
+import {ElMessageBox} from "element-plus";
+import Message from 'vue-m-message';
+import 'vue-m-message/dist/style.css';
 
 export const confirm = (msg: string): Promise<boolean> => {
     return new Promise((resolve, reject) => {
@@ -13,21 +15,58 @@ export const confirm = (msg: string): Promise<boolean> => {
         });
     });
 }
-export const message = (msg: string) => {
-    ElMessage(msg);
+/**
+ * 消息提
+ * @param msg 消息内容
+ * @param type 类型
+ * @param duration 持续时间
+ * @param title 标题
+ * @param position 位置
+ */
+export const message = (msg: string, type: string = "info", duration: number = 2000, title: string = "提示", position: string = "top-right") => {
+    Message({
+        message: msg,
+        type: type,
+        duration: duration,
+        title: title,
+        position: position,
+        supportHTML: true,
+        stopTimerOnHover: true,
+    });
 }
+/**
+ * 成功提示
+ * @param msg 消息内容
+ */
 export const success = (msg: string) => {
-    ElMessage({
-        message: msg,
-        type: 'success',
-    })
+    Message.success(msg, {
+        title: "成功",
+        supportHTML: true,
+        stopTimerOnHover: true,
+        position: "bottom-right"
+    });
 }
+/**
+ * 警告提示
+ * @param msg 消息内容
+ */
 export const warning = (msg: string) => {
-    ElMessage({
-        message: msg,
-        type: 'warning',
-    })
+    Message.warning(msg, {
+        title: "警告",
+        supportHTML: true,
+        stopTimerOnHover: true,
+        position: "bottom-right"
+    });
 }
+/**
+ * 错误提示
+ * @param msg 消息内容
+ */
 export const error = (msg: string) => {
-    ElMessage.error(msg)
+    Message.error(msg, {
+        title: "错误",
+        supportHTML: true,
+        stopTimerOnHover: true,
+        position: "bottom-right"
+    });
 }
