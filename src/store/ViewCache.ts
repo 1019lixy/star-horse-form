fe3feb537f008d28cf73b5f0f78b5dfbbacdde07
lixy <l_1019@163.com>
2025-03-19 08:01:44 +0800

    add code

--- src/store/ViewCache.ts
+++ src/store/ViewCache.ts
@@ -1,49 +1,46 @@
-import { defineStore } from "pinia";
-import { RouteRecordNormalized } from "vue-router";
-import { ref } from "vue";
+import {defineStore} from "pinia";
+import {RouteLocationNormalizedLoaded} from "vue-router";
+import {ref} from "vue";
 
 export const useViewCacheStore = defineStore(
-  "viewCache",
-  () => {
-    const viewListDatas = ref<Array<string>>([]);
+    "viewCache",
+    () => {
+        const viewListDatas = ref<Array<string>>([]);
 
-    /**
-     * 存视图
-     * @param data
-     */
-    const setViewCache = (data: RouteRecordNormalized) => {
-      const name = data.name as string;
-      if (viewListDatas.value.includes(name)) {
-        return;
-      }
-      viewListDatas.value.push(name);
-    };
-    /**
-     * 清除视图
-     * @param data
-     */
-    const clearCache = (data: RouteRecordNormalized) => {
-      const index = viewListDatas.value.findIndex((item) => item === data.name);
-      viewListDatas.value.splice(index, 1);
-    };
-    /**
-     * 清除所有Tab
-     */
-    const clearAll = () => {
-      // state.navBarList = [];
-      viewListDatas.value = [];
-    };
-    return {
-      viewListDatas,
-      setViewCache,
-      clearCache,
-      clearAll
-    };
-  },
-  {
-    persist: {
-      enabled: true,
-      strategies: [{ key: "viewListDatas", paths: ["viewListDatas"] }]
+        /**
+         * 存视图
+         * @param data
+         */
+        const setViewCache = (data: RouteLocationNormalizedLoaded) => {
+            const name = data.name as string;
+            if (viewListDatas.value.includes(name)) {
+                return;
+            }
+            viewListDatas.value.push(name);
+        };
+        /**
+         * 清除视图
+         * @param data
+         */
+        const clearCache = (data: RouteLocationNormalizedLoaded) => {
+            const index = viewListDatas.value.findIndex((item) => item === data.name);
+            viewListDatas.value.splice(index, 1);
+        };
+        /**
+         * 清除所有Tab
+         */
+        const clearAll = () => {
+            // state.navBarList = [];
+            viewListDatas.value = [];
+        };
+        return {
+            viewListDatas,
+            setViewCache,
+            clearCache,
+            clearAll
+        };
+    },
+    {
+        persist: true
     }
-  }
 );
