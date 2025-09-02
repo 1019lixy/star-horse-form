0aab414854b896c0c15def6eaef5c909447d8079
lixy <l_1019@163.com>
2025-09-02 15:36:58 +0800

    add code

--- src/views/dyform/FormPreview.vue
+++ src/views/dyform/FormPreview.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { Config } from "@/api/settings";
-import { computed, ref } from "vue";
-import { commonField } from "@/api/system";
-import { itemCheck } from "star-horse-lowcode";
+import {Config} from "@/api/settings";
+import {computed, ref} from "vue";
+import {commonField} from "@/api/system";
+import {itemCheck} from "star-horse-lowcode";
 
 const props = defineProps({
-  list: { type: Array<any>, required: true },
-  commonFieldList: { type: Array<any>, default: [] },
-  compSize: { type: String, default: Config.compSize },
-  formDisabled: { type: Boolean, default: false },
+  list: {type: Array<any>, required: true},
+  commonFieldList: {type: Array<any>, default: []},
+  compSize: {type: String, default: Config.compSize},
+  formDisabled: {type: Boolean, default: false},
 });
 
 const previewFormRef = ref<any>();
@@ -17,17 +17,17 @@ let commonFields = commonField();
 let fieldList = computed(() => {
   let tempList: any = [];
   props.commonFieldList
-    ?.filter((item) => item.formVisible)
-    .forEach((item) => {
-      let tempItem = commonFields.find(
-        (item2) => item2.fieldName == item.fieldName,
-      );
-      if (tempItem) {
-        tempItem["required"] = item.required;
-        tempItem["preps"] = { ...tempItem };
-        tempList.push(tempItem);
-      }
-    });
+      ?.filter((item) => item.formVisible)
+      .forEach((item) => {
+        let tempItem = commonFields.find(
+            (item2) => item2.fieldName == item.fieldName,
+        );
+        if (tempItem) {
+          tempItem["required"] = item.required;
+          tempItem["preps"] = {...tempItem};
+          tempList.push(tempItem);
+        }
+      });
   return tempList;
 });
 
@@ -64,37 +64,37 @@ defineExpose({
 <template>
   <div class="form-preview">
     <sh-form
-      :label-width="'auto'"
-      :label-position="'right'"
-      :require-asterisk-position="'right'"
-      :disabled="formDisabled"
-      :rules="{}"
-      v-model:dataForm="formData"
-      :size="compSize"
-      ref="previewFormRef"
+        :label-width="'auto'"
+        :label-position="'right'"
+        :require-asterisk-position="'right'"
+        :disabled="formDisabled"
+        :rules="{}"
+        v-model:dataForm="formData"
+        :size="compSize"
+        ref="previewFormRef"
     >
-      <el-scrollbar height="100%" style="width: 100% !important">
+      <div class="form-content-wrapper">
         <template v-for="data in list" :key="data.id">
           <component
-            :field="checkIsDisabled(data)"
-            v-model:formData="formData"
-            :isDesign="false"
-            :formInfo="list"
-            :showFormItem="true"
-            :is="itemCheck(data)"
+              :field="checkIsDisabled(data)"
+              v-model:formData="formData"
+              :isDesign="false"
+              :formInfo="list"
+              :showFormItem="true"
+              :is="itemCheck(data)"
           />
         </template>
         <template v-for="data in fieldList" :key="data.fieldName">
           <component
-            :field="checkIsDisabled(data)"
-            v-model:formData="formData"
-            :isDesign="false"
-            :showFormItem="true"
-            :formInfo="fieldList"
-            :is="itemCheck(data)"
+              :field="checkIsDisabled(data)"
+              v-model:formData="formData"
+              :isDesign="false"
+              :showFormItem="true"
+              :formInfo="fieldList"
+              :is="itemCheck(data)"
           />
         </template>
-      </el-scrollbar>
+      </div>
     </sh-form>
   </div>
 </template>
@@ -117,5 +117,13 @@ defineExpose({
   padding: 5px;
   flex: 1;
   overflow: hidden;
+}
+
+.form-content-wrapper {
+  width: 100%;
+  max-height: 100%;
+  overflow-x: hidden; // Remove horizontal scrollbar
+  overflow-y: auto; // Enable vertical scrollbar when needed
+  padding-right: 10px; // Prevent content cutoff due to scrollbar
 }
 </style>
