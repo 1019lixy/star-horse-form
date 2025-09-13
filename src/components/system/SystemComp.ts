feae36c55ef1eddc18e4053b5e5a84287bc89ef8
lixy <l_1019@163.com>
2025-09-13 16:23:46 +0800

    add code

--- src/components/system/SystemComp.ts
+++ src/components/system/SystemComp.ts
@@ -1,9 +1,11 @@
 import { App, defineAsyncComponent } from "vue";
 
+// @ts-ignore
 const items = import.meta.glob("@/components/system/*.vue");
 export default function install(app: App<Element>) {
   for (const [key, value] of Object.entries(items)) {
     const name = key.slice(key.lastIndexOf("/") + 1, key.lastIndexOf("."));
+    // @ts-ignore
     app.component(name, defineAsyncComponent(value));
   }
 }
